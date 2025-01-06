ecba86ea7fab7ec165f584bd112b48dae781b105
LowLifeCrisis <christopherllamas52@gmail.com>
2025-01-05 20:08:25 -0600

    Update script.js
    - Added a Alert if the player inputs a number lower than 0 or higher than 9

--- script.js
+++ script.js
@@ -9,26 +9,27 @@ function generateTarget() {
     randomNumber = Math.random() * 10;
     return Math.floor(randomNumber);
 }
-//Testing the GenerateTarget Function
-console.log(generateTarget());
 
 //Compare the secret number, Computer Number and the human entered number and pick a winner
 function compareGuesses(userGuess,computerGuess,secretNumber) {
+    
     //Subtract the guesses from the secret number to tell which one is closer, use Math.abs to return only absolute values
     let userDistance = Math.abs(secretNumber - userGuess);
     let computerDistance = Math.abs(secretNumber - computerGuess);
     // Start comparing the two values. Return true for user win, return false for computer win and if they tie return true for user win
-    if (userDistance < computerDistance) {
-        return true
+    if ((userGuess <= 0) || (userGuess >= 10)) {
+        return alert('Not 1-9')
     }
     else if (computerDistance < userDistance) {
         return false
     } else if ( userDistance === computerDistance) {
         return true
-    } else {
-        return 'Error'
+    } else if (userDistance < computerDistance) {
+        return true 
     }
 };
+
+
 // Check who won and update the score
 function updateScore(winner) {
     if (winner === 'human') {
@@ -40,6 +41,7 @@ function updateScore(winner) {
     }
 };
 
+//This is what advances the round counter
 function advanceRound() {
     currentRoundNumber = currentRoundNumber + 1
 }
